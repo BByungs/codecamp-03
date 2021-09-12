df47b3f6864cc92eaf29a57bb41bc3a9689846ed
BYEONGJIN-AHN <byungjin0120@gmail.com>
2021-09-12 18:56:44 +0900

    update freeboard

--- freeboard_frontend/src/components/units/boards/read-edit/BoardReadEdit.container.js
+++ freeboard_frontend/src/components/units/boards/read-edit/BoardReadEdit.container.js
@@ -28,6 +28,11 @@ export default function BoardReadEdit() {
             rating: 4,
           },
           password: editPasswordInput,
+          // id값 BoardReadEdit.presenter.js에서 받와야하는데
+          // 여긴 다른폴더라 값을 받아오지 못함
+          // 이걸 어떻게 받아오기만 하면 댓글수정 기능이 구현 될텐데
+          // 어떻게 해야할지 모르겠음
+          // 이부분 질문
           boardCommentId: router.query.detailPageNonMembersBasic,
         },
       });
