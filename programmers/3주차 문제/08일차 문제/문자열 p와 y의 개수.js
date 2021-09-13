1853bcd370f591e3e25bd01f739e79d9183a77bb
BYEONGJIN-AHN <byungjin0120@gmail.com>
2021-09-13 10:38:35 +0900

    3주차 첫번째날 알고리즘 풀이

--- programmers/3주차 문제/08일차 문제/문자열 p와 y의 개수.js
+++ programmers/3주차 문제/08일차 문제/문자열 p와 y의 개수.js
@@ -1,23 +1,58 @@
-function solution(s) {
+// function solution(s) {
 
-    let str = s.toLowerCase()
-    let arr =  str.split("")
-    // 문자열 "p" count 세주는 함수
-    let pCount = (arr) => {
-        return arr.filter(el=>el==="p").length
-    }
+//     let str = s.toLowerCase()
+//     let arr =  str.split("")
+//     // 문자열 "p" count 세주는 함수
+//     let pCount = (arr) => {
+//         return arr.filter(el=>el==="p").length
+//     }
 
-    // 문자열 "y" count 세주는 함수
-    let yCount = (arr) => {
-        return arr.filter(el=>el==="y").length
-    }
+//     // 문자열 "y" count 세주는 함수
+//     let yCount = (arr) => {
+//         return arr.filter(el=>el==="y").length
+//     }
 
-    return pCount(arr)===0 && yCount(arr) === 0 
-    ? true : pCount(arr) === yCount(arr) 
-    ? true : false 
-}
+//     return pCount(arr)===0 && yCount(arr) === 0 
+//     ? true : pCount(arr) === yCount(arr) 
+//     ? true : false 
+// }
 
 console.log(solution("pPoooyY"))
 console.log(solution("Pyy"))
 
 
+
+
+// function solution(s) {
+
+//     const check = {
+//         p:0,
+//         y:0
+//     }
+//     // p, y 의 개수를 저장 하는 객체
+
+//     s = s.toLowerCase()
+
+//     for(let i=0; i<s.length; i++) {
+//         if(s[i]==="p" || s[i]==="y") {
+//             check[s[i]]++
+//         }
+//     }
+
+//     return check.p === check.y ? true : false
+// }
+
+
+function solution(s) {
+
+    const check = {}
+    const result = s.toLowerCase()
+    .split("")
+    .forEach(str=>{
+        check[str] === undefined 
+        ? check[str] = 1
+        : check[str]++
+    })
+    
+    return check.p===check.y
+}
--- programmers/3주차 문제/08일차 문제/이상한 문자 만들기.js
+++ programmers/3주차 문제/08일차 문제/이상한 문자 만들기.js
@@ -1,21 +1,53 @@
-function solution(s) {
-    let newArr =  s.split(" ")
-    let emptyArr = []
-    for(let i=0; i<newArr.length; i++) {
-        let str = ""
-        for(let j=0; j<newArr[i].length; j++) {
-            if(j===0) {
-                str += newArr[i][j].toUpperCase()
-            } else if(j%2===0) {
-                str += newArr[i][j].toUpperCase()
-            } else {
-                str += newArr[i][j].toLowerCase()
-            }
-        }
-        emptyArr.push(str)
-        str = ""
-    }
-    return emptyArr.join(' ')
-}
+// function solution(s) {
+//     let newArr =  s.split(" ")
+//     let emptyArr = []
+//     for(let i=0; i<newArr.length; i++) {
+//         let str = ""
+//         for(let j=0; j<newArr[i].length; j++) {
+//             if(j===0) {
+//                 str += newArr[i][j].toUpperCase()
+//             } else if(j%2===0) {
+//                 str += newArr[i][j].toUpperCase()
+//             } else {
+//                 str += newArr[i][j].toLowerCase()
+//             }
+//         }
+//         emptyArr.push(str)
+//         str = ""
+//     }
+//     return emptyArr.join(' ')
+// }
 
 console.log(solution("hello world hAHa abcd"))
+
+// function solution(s) {
+//     let answer = ""
+
+//     let idx = 0;
+
+//     for(let i=0; i<s.length; i++) {
+//         if(s[i]===" ") {
+//             idx = 0;
+//             answer += " "
+//         } else {
+//             answer = answer + (idx %2 === 0 ? s[i].toLowerCase() : s[i].toUpperCase())
+//             idx += 1;
+//         }
+//     }
+//     return answer
+// }
+
+function solution(s) {
+    const answer = s.split(" ")
+    .map(str=>{
+        // console.log(str)
+        return str.split("").map((el,idx)=>{
+            if(idx%2===0) {
+                return el.toUpperCase()
+            } 
+            return el.toLowerCase()
+        }).join("")
+        
+    }).join(" ")
+    return answer
+}
