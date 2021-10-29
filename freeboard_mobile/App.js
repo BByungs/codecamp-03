68af50ae63deb7f4ce66daabf0a470121acebd41
BYEONGJIN-AHN <byungjin0120@gmail.com>
2021-10-29 12:14:59 +0900

    freeboard_mobile update

---
+++ freeboard_mobile/App.js
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import {WebView} from 'react-native-webview';
+
+
+const App: () => Node = () => {
+  return <WebView source={{uri: 'https://byungjins.site/' }} />;
+};
+
+
+
+export default App;
